15dbacd50c9f79a731fa12b384ab582a1ac2a520
Adityajain213 <jain07186@gmail.com>
2026-03-31 14:20:40 +0530

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,32 +1,43 @@
-# emotion_detection.py
-
-import requests  # Used to call Watson NLP API
+import requests
+import json
 
 def emotion_detector(text_to_analyze):
-    """
-    Sends input text to Watson NLP API and returns emotion scores.
-    """
-    if text_to_analyze.strip() == "":
-        return None
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(URL, json = input_json, headers=header)
+    formated_response = json.loads(response.text)
 
-
-    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
-
-    headers = {
-        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
-    }
-
-
-    input_json = {
-        "raw_document": {"text": text_to_analyze}
-    }
-
-    # API call
-    response = requests.post(url, json=input_json, headers=headers)
-
-    # Successful response
     if response.status_code == 200:
-        return response.json()["emotionPredictions"][0]["emotion"]
+        return formated_response
+    elif response.status_code == 400:
+        formated_response = {
+                            'anger': None,
+                            'disgust': None, 
+                            'fear': None, 
+                            'joy': None, 
+                            'sadness': None, 
+                            'dominant_emotion': None}
+        return formated_response
 
-    # Error case
-    return None
+def emotion_predictor(detected_text):
+    if all(value is None for value in detected_text.values()):
+        return detected_text
+    if detected_text['emotionPredictions'] is not None:
+        emotions = detected_text['emotionPredictions'][0]['emotion']
+        anger = emotions['anger']
+        disgust = emotions['disgust']
+        fear = emotions['fear']
+        joy = emotions['joy']
+        sadness = emotions['sadness']
+        max_emotion = max(emotions, key=emotions.get)
+        #max_emotion_score = emotions[max_emotion]
+        formated_dict_emotions = {
+                                'anger': anger,
+                                'disgust': disgust,
+                                'fear': fear,
+                                'joy': joy,
+                                'sadness': sadness,
+                                'dominant_emotion': max_emotion
+                                }
+        return formated_dict_emotions
