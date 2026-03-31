ebf18cd79945fd19583d2822a920e6813185457c
Adityajain213 <jain07186@gmail.com>
2026-03-31 14:16:13 +0530

    Implement emotion detection using Watson NLP API
    Added a function to detect emotions using Watson NLP API.

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,1 +1,32 @@
+# emotion_detection.py
 
+import requests  # Used to call Watson NLP API
+
+def emotion_detector(text_to_analyze):
+    """
+    Sends input text to Watson NLP API and returns emotion scores.
+    """
+    if text_to_analyze.strip() == "":
+        return None
+
+
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+
+
+    input_json = {
+        "raw_document": {"text": text_to_analyze}
+    }
+
+    # API call
+    response = requests.post(url, json=input_json, headers=headers)
+
+    # Successful response
+    if response.status_code == 200:
+        return response.json()["emotionPredictions"][0]["emotion"]
+
+    # Error case
+    return None
